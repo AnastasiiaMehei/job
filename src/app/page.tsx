da8ea547fc3f8f34898bfb351c23a2712f850c7e
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-09-03 20:51:46 +0300

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Header from '../components/Header';
-import { ToastContainer } from 'react-toastify';
+import '../styles/globals.css';
 
 export default function Home() {
   const [query, setQuery] = useState('');
@@ -10,9 +10,17 @@ export default function Home() {
   return (
     <>
       <Header />
-      
-      <ToastContainer />
-     
+      <div className="container mx-auto p-4">
+  <div className="relative overflow-hidden h-96 flex items-center justify-center">
+  
+    <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+    <div className="relative z-10 p-8 text-white flex items-center justify-center flex-col">
+  <h1 className="text-4xl font-bold mb-4">Welcome to Job Finder</h1>
+  <p className="text-xl mb-8">Find your dream job today!</p>
+</div>
+  </div>
+</div>
+
     </>
   );
 }
